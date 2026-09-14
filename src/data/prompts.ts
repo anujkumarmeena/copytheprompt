f5289a41ef4089432a3cf0d83fae2444bf2fdcb5
anujkumarmeena <60056096+anujkumarmeena@users.noreply.github.com>
2026-09-14 07:08:04 +0000

    feat: wire Pack 004 into gallery union (~74)

--- src/data/prompts.ts
+++ src/data/prompts.ts
@@ -2,6 +2,7 @@ import type { PromptItem } from "@/lib/types";
 import { indiaPrompts } from "./indiaPrompts";
 import { pack002Prompts } from "./pack002";
 import { pack003Prompts } from "./pack003";
+import { pack004Prompts } from "./pack004";
 import { archive022Prompts } from "./archive022";
 import { withHouseModel } from "./houseModel";
 
@@ -126,9 +127,13 @@ const HOUSE_MODEL_IDS = new Set([
   "meenakshi-corridor",
   "eid-moon-courtyard",
   "bihu-gamusa-portrait",
+  "desi-y2k-mall-mirror",
+  "pastel-tram-window",
+  "kurta-gym-hybrid",
+  "balcony-chai-monsoon",
 ]);
 
-export const prompts: PromptItem[] = [...pack003Prompts, ...pack002Prompts, ...indiaPrompts, ...archivePrompts, ...archive022Prompts].map((p) =>
+export const prompts: PromptItem[] = [...pack004Prompts, ...pack003Prompts, ...pack002Prompts, ...indiaPrompts, ...archivePrompts, ...archive022Prompts].map((p) =>
   HOUSE_MODEL_IDS.has(p.id) ? withHouseModel(p) : p
 );
 
